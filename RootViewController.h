a818f66436133e805d5f5e329d981dd37a97ee19
Dylan Jackson <dylan.r.jackson@gmail.com>
2012-07-11 23:40:40 -0700

    committing for github

---
+++ RootViewController.h
@@ -0,0 +1,18 @@
+//
+//  RootViewController.h
+//  RangeSlider
+//
+//  Created by Mal Curtis on 5/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RangeSlider.h"
+
+@interface RootViewController : UITableViewController{
+    NSString * _sliderRangeText;
+}
+
+-(void)updateRangeLabel:(RangeSlider *)slider;
+
+@end
